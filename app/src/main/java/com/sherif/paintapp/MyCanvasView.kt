340e1a31701282c9d97beeb9a5926dddc9cafe97
Shiro <sherifmohamed2004@gmail.com>
2020-02-18 18:57:48 +0200

    first commit

---
+++ app/src/main/java/com/sherif/paintapp/MyCanvasView.kt
@@ -0,0 +1,25 @@
+package com.sherif.paintapp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+
+
+
+class MyCanvasView (context: Context) :View(context){
+
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+    }
+
+}
